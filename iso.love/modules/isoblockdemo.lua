8e906ec8ed1f28f8674c2b28580186c26c30164c
David Crosby <dcrosby@llnw.com>
2017-05-17 21:52:10 -0400

    moveable blocks

--- iso.love/modules/isoblockdemo.lua
+++ iso.love/modules/isoblockdemo.lua
@@ -1,12 +1,19 @@
 local Colors = require 'colors'
 local IsoBlock = require 'isoblock'
-local function newBlock(pos,size,color)
+local function newBlock(pos,size,color,name)
   return {
     type="block",
     pos=pos,
     size=size,
-    color=color
+    color=color,
+    name=name
   }
+end
+local function printBlocks(blocks)
+  print("Blocks:")
+  for i=1,#blocks do
+    print("  "..i..": "..blocks[i].name)
+  end
 end
 
 local function newWorld()
@@ -15,30 +22,69 @@ local function newWorld()
   --   newBlock({x=2,y=4,z=0},{x=2.3,y=2,z=1},Colors.Green),
   -- }
   local blocks = {
-    newBlock({x=2,y=2,z=0},{x=1,y=1,z=1.5}, Colors.Red),
-    newBlock({x=3,y=1,z=0},{x=1,y=4,z=1},   Colors.Blue),
-    newBlock({x=1,y=3,z=0},{x=2,y=2,z=2.5}, Colors.Green),
+    newBlock({x=2,y=2,z=0},{x=1,y=1,z=1.5}, Colors.Red, "red"),
+    newBlock({x=3,y=1,z=0},{x=1,y=4,z=1},   Colors.Blue, "blue"),
+    newBlock({x=1,y=3,z=0},{x=2,y=2,z=2.5}, Colors.Green, "green"),
   }
+  printBlocks(blocks)
 
   blocks = IsoBlock.sortBlocks(blocks)
+  printBlocks(blocks)
   local model ={
     viewoff={x=400,y=300},
     blocks = blocks,
+    blockIndex = lcopy(blocks),
+    selectedBlock = 1,
   }
+  printBlocks(model.blockIndex)
   return model
 
 end
 
+local function wasd(dir,model,action)
+  local block = model.blockIndex[model.selectedBlock]
+  if dir == "up" then
+    if action.shift then
+      block.pos.z = block.pos.z + 1
+    else
+      block.pos.y = block.pos.y + 1
+    end
+  elseif dir == "down" then
+    if action.shift then
+      block.pos.z = block.pos.z - 1
+    else
+      block.pos.y = block.pos.y - 1
+    end
+  elseif dir == "left" then
+    block.pos.x = block.pos.x - 1
+  elseif dir == "right" then
+    block.pos.x = block.pos.x + 1
+  end
+  print("wasd "..block.name.." "..block.pos.x..","..block.pos.y..","..block.pos.z)
+  model.blocks = IsoBlock.sortBlocks(model.blocks)
+end
+
 local function updateWorld(model,action)
-  if action.type == "crozeng.reloadError" then
+  if action.type == "keyboard" and action.state == "pressed" then
+
+    if action.key == 'w' then wasd('up',model,action)
+    elseif action.key == 's' then wasd('down',model,action)
+    elseif action.key == 'a' then wasd('left',model,action)
+    elseif action.key == 'd' then wasd('right',model,action)
+
+    elseif action.key == 'r' then
+      return model, {{type="crozeng.reloadRootModule"}}
+    elseif action.key == 'space' then
+      model.selectedBlock = model.selectedBlock + 1
+      if model.selectedBlock > #model.blockIndex then
+        model.selectedBlock = 0
+      end
+    end
+
+  elseif action.type == "crozeng.reloadError" then
     model.reloadError = "DANG"
   elseif action.type == "crozeng.reloadOk" then
     model.reloadError = nil
-  end
-  if action.type == "keyboard" and action.state == "pressed" then
-    if action.key == 'r' then
-      return model, {{type="crozeng.reloadRootModule"}}
-    end
   end
   return model, nil
 end
@@ -129,6 +175,8 @@ local function drawWorld(model)
   end
 
   love.graphics.pop()
+
+  love.graphics.print("selected: "..model.blockIndex[model.selectedBlock].name)
 
 
 
